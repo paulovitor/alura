58fa4042b6c67ee84e8e45813562eafb236b3c79
Paulo <paulo.vitor@aisdigital.com.br>
2017-11-08 18:15:17 -0200

    Introdução à Threads

---
+++ threads-java-1/threads/src/br/com/alura/threads/Principal.java
@@ -0,0 +1,10 @@
+package br.com.alura.threads;
+
+public class Principal {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Thread MAIN");
+		
+		Thread.sleep(50000);
+	}
+}
