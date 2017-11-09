6b55c1717ddf3072f08e2d5b04a1f61df116a594
Paulo <paulo.vitor@aisdigital.com.br>
2017-11-09 14:50:14 -0200

    Revisitando a lista

--- threads-java-1/threads/src/br/com/alura/threads/Principal.java
+++ threads-java-1/threads/src/br/com/alura/threads/Principal.java
@@ -1,24 +1,15 @@
 package br.com.alura.threads;
 
-import java.util.List;
-import java.util.Vector;
-
 public class Principal {
 
 	public static void main(String[] args) throws InterruptedException {
+		Lista lista = new Lista();
 
-//		List<String> lista = Collections.synchronizedList(new ArrayList<String>());
-		List<String> lista = new Vector<String>();
+        for (int i = 0; i < 10; i++) {
+            new Thread(new TarefaAdicionarElemento(lista, i)).start();
+        }
 
-		for (int i = 0; i < 10; i++) {
-			new Thread(new TarefaAdicionarElemento(lista, i)).start();
-		}
-
-		Thread.sleep(2000);
-
-		for (int i = 0; i < lista.size(); i++) {
-			System.out.println(i + " - " + lista.get(i)); // utilizando get(i)
-		}
+        new Thread(new TarefaImprimir(lista)).start();
 	}
 	
 }
--- threads-java-1/threads/src/br/com/alura/threads/TarefaAdicionarElemento.java
+++ threads-java-1/threads/src/br/com/alura/threads/TarefaAdicionarElemento.java
@@ -1,13 +1,11 @@
 package br.com.alura.threads;
 
-import java.util.List;
-
 public class TarefaAdicionarElemento implements Runnable {
 
-	private List<String> lista;
+	private Lista lista;
 	private int numeroDoThread;
 
-	public TarefaAdicionarElemento(List<String> lista, int numeroDoThread) {
+	public TarefaAdicionarElemento(Lista lista, int numeroDoThread) {
 		this.lista = lista;
 		this.numeroDoThread = numeroDoThread;
 	}
@@ -15,7 +13,7 @@ public class TarefaAdicionarElemento implements Runnable {
 	@Override
 	public void run() {
 		for (int i = 0; i < 100; i++) {
-			lista.add("Thread " + numeroDoThread + " - " + i);
+			lista.adicionaElementos("Thread " + numeroDoThread + " - " + i);
 		}
 	}
 
