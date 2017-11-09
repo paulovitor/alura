e3a806514ca3c7079a1457c42b992c6eaee1164f
Paulo <paulo.vitor@aisdigital.com.br>
2017-11-09 13:54:39 -0200

    Coleções thread-safe

--- threads-java-1/threads/src/br/com/alura/threads/Principal.java
+++ threads-java-1/threads/src/br/com/alura/threads/Principal.java
@@ -1,10 +1,24 @@
 package br.com.alura.threads;
 
+import java.util.List;
+import java.util.Vector;
+
 public class Principal {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println("Thread MAIN");
-		
-		Thread.sleep(50000);
+
+//		List<String> lista = Collections.synchronizedList(new ArrayList<String>());
+		List<String> lista = new Vector<String>();
+
+		for (int i = 0; i < 10; i++) {
+			new Thread(new TarefaAdicionarElemento(lista, i)).start();
+		}
+
+		Thread.sleep(2000);
+
+		for (int i = 0; i < lista.size(); i++) {
+			System.out.println(i + " - " + lista.get(i)); // utilizando get(i)
+		}
 	}
+	
 }
---
+++ threads-java-1/threads/src/br/com/alura/threads/TarefaAdicionarElemento.java
@@ -0,0 +1,22 @@
+package br.com.alura.threads;
+
+import java.util.List;
+
+public class TarefaAdicionarElemento implements Runnable {
+
+	private List<String> lista;
+	private int numeroDoThread;
+
+	public TarefaAdicionarElemento(List<String> lista, int numeroDoThread) {
+		this.lista = lista;
+		this.numeroDoThread = numeroDoThread;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 100; i++) {
+			lista.add("Thread " + numeroDoThread + " - " + i);
+		}
+	}
+
+}
