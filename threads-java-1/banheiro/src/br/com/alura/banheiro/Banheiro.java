dfabb080ac25f637a8112773f624b0b33efe28f0
Paulo <paulo.vitor@aisdigital.com.br>
2017-11-09 14:32:43 -0200

    Espere e notifique

--- threads-java-1/banheiro/src/br/com/alura/banheiro/Banheiro.java
+++ threads-java-1/banheiro/src/br/com/alura/banheiro/Banheiro.java
@@ -1,35 +1,34 @@
 package br.com.alura.banheiro;
 
-//import java.util.concurrent.locks.Lock;
-//import java.util.concurrent.locks.ReentrantLock;
-
 public class Banheiro {
 
-//	private Lock lock = new ReentrantLock();
-	
+	private boolean ehSujo = true;
+
 	public void fazNumero1() {
 		String nome = Thread.currentThread().getName();
 
 		System.out.println(nome + " batendo na porta");
 
-//		lock.lock();
-		
 		synchronized (this) {
 			System.out.println(nome + " entrando no banheiro");
+
+			while (this.ehSujo)
+				esperaLaFora(nome);
+
 			System.out.println(nome + " fazendo coisa rapida");
-	
+
 			try {
 				Thread.sleep(8000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-	
+			
+			this.ehSujo = true;
+
 			System.out.println(nome + " dando descarga");
 			System.out.println(nome + " lavando a mao");
 			System.out.println(nome + " saindo do banheiro");
 		}
-			
-//		lock.unlock();
 	}
 
 	public void fazNumero2() {
@@ -39,6 +38,10 @@ public class Banheiro {
 
 		synchronized (this) {
 			System.out.println(nome + " entrando no banheiro");
+
+			while (this.ehSujo)
+				esperaLaFora(nome);
+
 			System.out.println(nome + " fazendo coisa demorada");
 
 			try {
@@ -46,9 +49,49 @@ public class Banheiro {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			
+			this.ehSujo = true;
 
 			System.out.println(nome + " dando descarga");
 			System.out.println(nome + " lavando a mao");
+			System.out.println(nome + " saindo do banheiro");
+		}
+	}
+
+	private void esperaLaFora(String nome) {
+		System.out.println(nome + ", eca, banheiro está sujo");
+		try {
+			this.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void limpa() {
+		String nome = Thread.currentThread().getName();
+
+		System.out.println(nome + " batendo na porta");
+
+		synchronized (this) {
+
+			System.out.println(nome + " entrando no banheiro");
+
+			if (!this.ehSujo) {
+				System.out.println(nome + ", não está sujo, vou sair");
+				return;
+			}
+
+			System.out.println(nome + " limpando o banheiro");
+			this.ehSujo = false;
+
+			try {
+				Thread.sleep(13000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			this.notifyAll();
+
 			System.out.println(nome + " saindo do banheiro");
 		}
 	}
