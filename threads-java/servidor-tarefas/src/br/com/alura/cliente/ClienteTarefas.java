7266b9250043dcfe66d766c5d87ea09f0ed69f7b
Paulo <paulo.vitor@aisdigital.com.br>
2017-11-10 14:00:23 -0200

    O projeto Servidor de tarefas

---
+++ threads-java/servidor-tarefas/src/br/com/alura/cliente/ClienteTarefas.java
@@ -0,0 +1,18 @@
+package br.com.alura.cliente;
+
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClienteTarefas {
+
+	public static void main(String[] args) throws Exception {
+		Socket socket = new Socket("localhost", 12345);
+		System.out.println("Conexão Estabelecida");
+
+		Scanner teclado = new Scanner(System.in);
+        teclado.nextLine();
+
+        teclado.close();
+        socket.close();
+	}
+}
