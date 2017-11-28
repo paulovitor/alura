a1fbb43e836b078199e876ed9bfbc442f31449ad
Paulo <paulo.vitor@aisdigital.com.br>
2017-11-28 11:31:37 -0200

    Lidando com formulários

--- vuejs/alurapic/src/components/cadastro/Cadastro.vue
+++ vuejs/alurapic/src/components/cadastro/Cadastro.vue
@@ -3,18 +3,79 @@
 <template>
   <div>
     <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado">{{ foto.titulo }}</h2>
+
+    <form @submit.prevent="gravar()">
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <input :value="foto.titulo" @input="foto.titulo = $event.target.value" id="titulo" autocomplete="off">
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input :value="foto.url" @input="foto.url = $event.target.value" id="url" autocomplete="off">
+        <imagem-responsiva/>
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea :value="foto.descricao" @input="foto.descricao = $event.target.value" id="descricao" autocomplete="off"></textarea>
+      </div>
+
+      <div class="centralizado">
+        <meu-botao rotulo="GRAVAR" tipo="submit"/>
+        <router-link to="/"><meu-botao rotulo="VOLTAR" tipo="button"/></router-link>
+      </div>
+
+    </form>
   </div>
 </template>
 
 <script>
+import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
+import Botao from "../shared/botao/Botao.vue";
 
 export default {
 
-}
-
-</script>
-<style scoped>
-    .centralizado {
-        text-align: center;
+  components: {
+    "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao
+  },
+  data() {
+    return {
+      foto: {
+        titulo: "",
+        url: "",
+        descricao: ""
+      }
+    };
+  },
+  methods: {
+    gravar() {
+      console.log(this.foto);
+      this.foto = {};
     }
+  }
+};
+</script>
+
+<style scoped>
+  .centralizado {
+    text-align: center;
+  }
+  .controle {
+    font-size: 1.2em;
+    margin-bottom: 20px;
+  }
+  .controle label {
+    display: block;
+    font-weight: bold;
+  }
+
+  .controle label + input,
+  .controle textarea {
+    width: 100%;
+    font-size: inherit;
+    border-radius: 5px;
+  }
 </style>
