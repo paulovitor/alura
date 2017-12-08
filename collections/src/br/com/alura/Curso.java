4d44f689c1cc8c0adf9ddb4fd0eee15263c2dd42
Paulo <paulo.vitor@aisdigital.com.br>
2017-12-08 16:31:14 -0200

    Mapas

--- collections/src/br/com/alura/Curso.java
+++ collections/src/br/com/alura/Curso.java
@@ -1,9 +1,11 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Curso {
@@ -12,8 +14,12 @@ public class Curso {
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
 	private Set<Aluno> alunos = new HashSet<>(); // não guarda a ordem de inserção
-//	private Set<Aluno> alunos = new LinkedHashSet<>(); // guarda a ordem de inserção
-//	private Set<Aluno> alunos = new TreeSet<>(); // guarda a ordem natural (mas é preciso que os elementos implements Comparable)
+	// private Set<Aluno> alunos = new LinkedHashSet<>(); // guarda a ordem de inserção
+	// private Set<Aluno> alunos = new TreeSet<>(); // guarda a ordem natural (mas é
+	// preciso que os elementos implements Comparable)
+	private Map<Integer, Aluno> matriculaParaAluno = new HashMap<>();
+//	private Map<Integer, Aluno> matriculaParaAluno = new LinkedHashMap<>(); // guarda a ordem de inserçao
+//	private Map<Integer, Aluno> matriculaParaAluno = new HashTable<>(); // thread-safe
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -46,6 +52,7 @@ public class Curso {
 
 	public void matricula(Aluno aluno) {
 		this.alunos.add(aluno);
+		this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
 	}
 
 	@Override
@@ -56,5 +63,10 @@ public class Curso {
 
 	public boolean estaMatriculado(Aluno aluno) {
 		return this.alunos.contains(aluno);
+	}
+
+	public Aluno buscaMatriculado(int matricula) {
+		return this.matriculaParaAluno.get(matricula);
+
 	}
 }
