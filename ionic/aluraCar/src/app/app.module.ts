5e97dc34bb2a30ad06e67728a8428f79eaa3faeb
Paulo <paulo.vitor@aisdigital.com.br>
2017-12-21 13:16:32 -0200

    Consumindo a API do cliente

--- ionic/aluraCar/src/app/app.module.ts
+++ ionic/aluraCar/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    BrowserModule,
+    HttpModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
