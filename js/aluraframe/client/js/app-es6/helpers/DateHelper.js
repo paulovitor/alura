837117537cb78272af04a962b2e0c10143f0c094
Paulo <paulovitor88@gmail.com>
2018-01-14 17:22:24 -0200

    Curso JavaScript avançado
    ES6, orientação a objetos e padrões de projetos

---
+++ js/aluraframe/client/js/app-es6/helpers/DateHelper.js
@@ -0,0 +1,16 @@
+export class DateHelper {
+
+    construtor() {
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static textoParaData(texto) {
+        if (!/\d{2}\/\d{2}\/\d{4}/.test(texto))
+            throw new Error('Deve estar no formato dd/mm/aaaa');
+        return new Date(...texto.split('/').reverse().map((item, indice) => item - indice % 2));
+    }
+
+    static dataParaTexto(data) {
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+    }
+}
