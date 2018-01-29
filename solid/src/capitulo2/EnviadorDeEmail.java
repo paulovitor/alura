7cf3fa3a8b12aba10d165117f42a60a5f50d2e56
Paulo <paulo.vitor@aisdigital.com.br>
2018-01-29 10:58:47 -0200

    Acoplamento e a estabilidade

---
+++ solid/src/capitulo2/EnviadorDeEmail.java
@@ -0,0 +1,8 @@
+package capitulo2;
+
+public class EnviadorDeEmail {
+
+	public void enviaEmail(NotaFiscal nf) {
+		System.out.println("envia email da nf " + nf.getId());
+	}
+}
---
+++ solid/src/capitulo2/NotaFiscalDao.java
@@ -0,0 +1,8 @@
+package capitulo2;
+
+public class NotaFiscalDao {
+
+	public void persiste(NotaFiscal nf) {
+        System.out.println("salva nf no banco");
+    }
+}
