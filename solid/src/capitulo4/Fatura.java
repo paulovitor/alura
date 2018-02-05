0c37e934091d94e10f19a4c23649ae93c0f7898d
Paulo <paulo.vitor@aisdigital.com.br>
2018-02-05 15:07:04 -0200

    Entendendo o encapsulamento

--- solid/src/capitulo4/Fatura.java
+++ solid/src/capitulo4/Fatura.java
@@ -1,39 +1,52 @@
 package capitulo4;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Fatura {
 
-    private String cliente;
-    private double valor;
-    private List<Pagamento> pagamentos;
-    private boolean pago;
+	private String cliente;
+	private double valor;
+	private List<Pagamento> pagamentos;
+	private boolean pago;
 
-    public Fatura(String cliente, double valor) {
-        this.cliente = cliente;
-        this.valor = valor;
-        this.pagamentos = new ArrayList<Pagamento>();
-        this.setPago(false);
-    }
+	public Fatura(String cliente, double valor) {
+		this.cliente = cliente;
+		this.valor = valor;
+		this.pagamentos = new ArrayList<Pagamento>();
+		this.pago = false;
+	}
 
-    public String getStringCliente() {
-        return cliente;
-    }
+	public void adicionaPagamento(Pagamento pagamento) {
+		this.pagamentos.add(pagamento);
+		if (valorTotalDosPagamentos() >= this.valor) {
+			this.pago = true;
+		}
+	}
 
-    public double getValor() {
-        return valor;
-    }
+	private double valorTotalDosPagamentos() {
+		double total = 0;
 
-    public List<Pagamento> getPagamentos() {
-        return pagamentos;
-    }
+		for (Pagamento p : pagamentos) {
+			total += p.getValor();
+		}
+		return total;
+	}
 
-    public boolean isPago() {
-        return pago;
-    }
+	public String getStringCliente() {
+		return cliente;
+	}
 
-	public void setPago(boolean pago) {
-		this.pago = pago;
+	public double getValor() {
+		return valor;
+	}
+
+	public List<Pagamento> getPagamentos() {
+		return Collections.unmodifiableList(pagamentos);
+	}
+
+	public boolean isPago() {
+		return pago;
 	}
 }
