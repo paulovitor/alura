1d2f937709c62d3029049d5eb1a96c3e6952fe2f
Paulo <paulovitor88@gmail.com>
2018-03-14 22:55:49 -0300

    Testes end*to*end com JAX*RS e Grizzly

--- webservices/webservices-rest-com-jaxrs-e-jersey/src/main/java/br/com/alura/loja/modelo/Projeto.java
+++ webservices/webservices-rest-com-jaxrs-e-jersey/src/main/java/br/com/alura/loja/modelo/Projeto.java
@@ -4,18 +4,22 @@ import com.thoughtworks.xstream.XStream;
 
 public class Projeto {
 
+	private long id;
+	private String nome;
+	private int anoDeInicio;
+
 	public Projeto(long id, String nome, int anoDeInicio) {
 		this.id = id;
 		this.nome = nome;
 		this.anoDeInicio = anoDeInicio;
 	}
 
-	private long id;
-	private String nome;
-	private int anoDeInicio;
-
 	public void setId(long id) {
 		this.id = id;
+	}
+
+	public Object getNome() {
+		return nome;
 	}
 
 	public String toXML() {
