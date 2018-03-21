1265ec952e1682a21aa4803f8de474d5ec8a609a
Paulo <paulovitor88@gmail.com>
2018-03-20 23:03:35 -0300

    Utilizando filtros e logging

--- webservices/webservices-rest-com-jaxrs-e-jersey/src/test/java/br/com/alura/loja/CarrinhoTest.java
+++ webservices/webservices-rest-com-jaxrs-e-jersey/src/test/java/br/com/alura/loja/CarrinhoTest.java
@@ -8,6 +8,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -28,7 +30,10 @@ public class CarrinhoTest {
 	public void startaServidor() {
 		server = Servidor.inicializaServidor();
 
-		client = ClientBuilder.newClient();
+		ClientConfig config = new ClientConfig();
+		config.register(new LoggingFilter());
+
+		client = ClientBuilder.newClient(config);
 		target = client.target("http://localhost:8080");
 	}
 
